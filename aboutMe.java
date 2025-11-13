e0fb50778baccd949d77cb8ef6fea019c15888b2
rasoolsiddik <rasoolsiddik8@gmail.com>
2025-11-13 19:48:34 +0530

    Add files via upload

---
+++ aboutMe.java
@@ -0,0 +1,12 @@
+public class aboutMe { 
+    public static void main(String[] args) {
+        String name = "Rasool S";
+        int age = 19;
+        String language = "Java";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Favorite Language: " + language);
+}
+
+    
+}
---
+++ circleArea.java
@@ -0,0 +1,9 @@
+public class circleArea {
+      public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle: " + area);
+    }
+  
+    
+}
